f754bca464bccd5bab39ab2dab3c1af57e101c7b
Gabriel Ferrer <gabrielferrergtf@gmail.com>
2019-08-20 04:28:05 -0400

    applied assume-unchanged to dao folder

--- src/main/java/main/Crypt.java
+++ src/main/java/main/Crypt.java
@@ -1,22 +1,24 @@
 package main;
 
 import java.util.Random;
+
 /**
  * Used to encrypt and decrypt a password
+ * 
  * @author devon
  *
  */
 
 class Crypt {
-	
+
 	private Crypt() {
 		super();
 	}
-	
+
 	static String decryptWord(String word) {
 		return decrypt(word);
 	}
-	
+
 	static String encryptWord(String word) {
 		return encrypt(word);
 	}
@@ -60,5 +62,19 @@ class Crypt {
 			str += (char) (((int) word.charAt(i) + 56) / 2);
 		}
 		return str;
+
 	}
+
+	public static void main(String[] args) {
+		String[] unenc = { "SngprFV04A@", "qRiMJzqfTaHh!", "xaHDihEd$", "GL3ckD2E!", "yF5hOQavg@", "dPZN6MfOkYhd@",
+				"fXgpE2B!", "ZkEiY2vun1!", "8YB1VvCN8I!", "svHjnlK3ka6!" };
+		String [] decryp = { "CV_,|²Adr%\"q¬¾Âj?>FDvJHB+31sH>","C!y#¸z-w4|w5°xrÒÀª? n*s?zHXq!Bk","?ZPXÆ?~q?J<Vnf°®hiG?zX@yM5f"
+				,"@tLbdnXEzyL&D¤´fB0#R_YgzbGv","B:K{Èb`mLVhEH®|? Ê{f?H|q=?;I/",".0?4?vr3b'G??zJxª|´?®|Y?HUR?].Zz"
+				,"N(4b¢?pR#2=?¬¾hB,L~?S9AM_","/y9M?¬?3y^c~h°?BÊÈºuZ*?b'/'L{","wx/F?2QO,B0b@?ÊdzN}kZ*nap1V~","??,=¼Â+ix{r'n²º¶tD´ ?e45tm&LKJ"};
+		for(int i = 0; i < decryp.length; i++) {
+			
+			System.out.println( decrypt(decryp[i]) == unenc[i]);
+		}
+	}
+
 }
